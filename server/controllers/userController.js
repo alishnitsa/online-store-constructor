65bcceba48157ffd0ad07b0e327acb14dc2928a1
Alish <alish.gilenov@mail.ru>
2021-04-12 17:01:34 +0300

    created registration, login, jwt token and bcrypt

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,12 +1,45 @@
 const ApiError = require('../error/ApiError')
+const bcrypt = require('bcrypt') // Для хеширования паролей
+const jwt = require('jsonwebtoken') // Токен
+const { User, Basket } = require('../models/models') // Модели пользователя и корзины
+
+const generateJwt = (id, email, role) => { // Генерация токена
+	return jwt.sign(
+		{ id, email, role }, // Данные
+		process.env.SECRET_KEY, // Секретный ключ
+		{ expiresIn: '24h' } // Время жизни токена
+	)
+}
 
 class UserController { // Контроллер для пользователя
-	async registration(req, res) { // Регистрация
-
+	async registration(req, res, next) { // Регистрация
+		const { email, password, role } = req.body // Получение из тела запроса email, пароль и роль
+		if (!email && !password) { // Если поля логина и пароля пустые, то возвращаем ошибку
+			return next(ApiError.badRequest('Некорректный email или пароль'))
+		}
+		const candidate = await User.findOne({ where: { email } }) // Проверка существования пользователя в системе
+		if (candidate) {
+			return next(ApiError.badRequest('Пользователь с таким email уже существует'))
+		}
+		const hashPassword = await bcrypt.hash(password, 5) // Хеширование пароля, если пользователя не существует (пароль, кол-во хеширований)
+		const user = await User.create({ email, role, password: hashPassword }) // Создание пользователя
+		const basket = await Basket.create({ userId: user.id }) // Создание корзины для пользователя
+		const token = generateJwt(user.id, user.email, user.role) // Создание токена
+		return res.json({ token })
 	}
 
-	async login(req, res) { // Вход
-
+	async login(req, res, next) { // Вход
+		const { email, password } = req.body // Получение email и пароля
+		const user = await User.findOne({ where: { email } }) // Проверка на существования пользователя в БД
+		if (!user) {
+			return next(ApiError.badRequest('Пользователь не найден'))
+		}
+		let comparePassword = bcrypt.compareSync(password, user.password) // Сравнение введенного пароля и зашифрованного в БД
+		if (!comparePassword) {
+			return next(ApiError.badRequest('Указан неверный пароль'))
+		}
+		const token = generateJwt(user.id, user.email, user.role) // Создание токена
+		return res.json({ token })
 	}
 
 	async check(req, res, next) { // Проверка
